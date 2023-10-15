b6f3a65490f44cc2c9cba42a16aa69aec962ead1
NWOM <98202572+NWOM@users.noreply.github.com>
2023-10-15 22:31:52 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
+const path = require('path');
+const HWP=require('html-webpack-plugin')
 module.exports = {
-	entry: './script.js',
-	output: {
-		path: './',
-		filename: 'transpiled.js',
-	},
-	module: {
-		loaders: [
-			   { 
-			   	test: /\.js$/,
-			   	loader: 'babel-loader', 
-			   	exclude: /node_modules/, 
-			   	query: {
-			   		presets: ['es2015','react']
+  entry: './script.js',
+  output: {
+    path: path.resolve(__dirname, 'C:\\Users\\suman\\OneDrive\\Desktop\\REACT JS', 'dist'),
+    filename: 'transpiled.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HWP({template: path.resolve('./index.html')}),
 
-			   } 
-			}
-
-		]
-	}
+    ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  mode: 'development',
 }
+
